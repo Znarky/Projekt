46a4e6d7a3e6c5465d2f256680a231e64ec2f0ef
Znarky <svante.noltorp@hotmail.com>
2015-04-08 16:48:04 +0200

    Ny kommentring

--- TestProjekt/TestProjekt/Test.cpp
+++ TestProjekt/TestProjekt/Test.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 int main(){
+	// Detta är ännu en förändring men med kommentar istället
 	std::cout << "Detta är ett test projekt" << std::endl;
 	std::cout << "En ny förändring i projektet" << std::endl;
 	return 0;
